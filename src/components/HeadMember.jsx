83c0606cd5236af4a1210993f4fec91d034fad2c
Pradeep Anand <panandinnitp@gmail.com>
2024-12-08 15:19:14 +0530

    Image Error Solve

--- src/components/HeadMember.jsx
+++ src/components/HeadMember.jsx
@@ -6,7 +6,7 @@ function HeadMember() {
         <div class="col">
           <div class="head-img card" id="nitish">
             <img
-              src="./public/nitish-kumar.jpg"
+              src="/public/nitish-kumar.jpg"
               class="card-img-top size-52"
               alt="..."
             />
